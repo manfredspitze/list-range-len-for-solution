04b92bfcbe94587d707ef33e5d7b2b16b79cedb8
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-22 11:47:40 -0400

    Create list-len-function.py

---
+++ list-len-function.py
@@ -0,0 +1,9 @@
+# Abe Lincoln
+# 20 SEP 20XX
+# List & len( ) Function
+
+items = ["apples", "milk", "bread", "eggs", "cheese"]
+
+num_items = len(items)
+
+print(f'Your list contains {num_items} items.')
