80c73b86b3e278af3a5173b81a11e1a0bc26e47d
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-22 08:17:43 -0400

    Update display_list_index_nums_solution.py

---
+++ display_list_index_nums_solution.py
@@ -0,0 +1,8 @@
+# Abe Lincoln
+# 20 SEP 20XX
+# Display Index Number of List Item
+
+grocery_items = ["apples", "milk", "bread", "eggs", "cheese"]
+
+for i in range(len(grocery_items)):
+    print(f"Item {i + 1}: {grocery_items[i]}")
