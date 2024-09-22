1ac20d35f75ffeb2042cf66640bc34957d4569e3
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-22 11:54:38 -0400

    Update list-display-item.py

--- list-display-item.py
+++ list-display-item.py
@@ -1,1 +1,10 @@
+# Abe Lincoln
+# 20 SEP 20XX
+# Printing List Items
 
+items = ["apples", "milk", "bread", "eggs", "cheese"]
+print(f"{items[2]} has an index of: { items.index('bread') }")
+print(f"{items[4]} has an index of: { items.index('cheese') }")
+print(f"{items[1]} has an index of: { items.index('milk') }")
+print(f"{items[0]} has an index of: { items.index('apples') }")
+print(f"{items[3]} has an index of: { items.index('eggs') }")
